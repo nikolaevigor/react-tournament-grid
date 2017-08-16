9069c299eb4e9688244652d83a4313e443b8346c
Igor Nikolaev <oldsantiago21@gmail.com>
2017-08-16 03:55:53 +0300

    chore: fix exporting

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -2,17 +2,17 @@ export function range(target) {
   return Array.from({ length: target }, (_, idx) => idx);
 }
 
-function getScoreFromGames(games) {
+export function getScoreFromGames(games) {
   const firstTeamScore = games.filter(g => g.first_team_result === 'win');
   const secondTeamScore = games.filter(g => g.second_team_result === 'win');
   return [firstTeamScore.length, secondTeamScore.length];
 }
 
-function findMaxStage(seriesList) {
+export function findMaxStage(seriesList) {
   return Math.max(...seriesList.map(series => series.stage));
 }
 
-function findSeries(stage, order, seriesList) {
+export function findSeries(stage, order, seriesList) {
   return (
     seriesList.filter(
       series => series.stage === stage && series.order === order
