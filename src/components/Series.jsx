414f88670aafbf707f8fae55a882e8d2d73cd504
Igor Nikolaev <oldsantiago21@gmail.com>
2017-08-17 03:11:55 +0300

    fix: change colors

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -23,20 +23,27 @@ export default ({
     style={{ cursor: 'pointer' }}
   >
     <rect x="0" y="0" width={width} height={height} fill="#58595e" />
-    <rect x="0" y="0" width={width} height={height / 2} fill="#58595e" />
+    <rect x="0" y="0" width={width} height={height / 2} fill="#2f2f2f" />
     <rect
       x="0"
       y={height / 2}
       width={width}
       height={height / 2}
-      fill="#58595e"
+      fill="#252525"
     />
     <rect
       x={width - scoreWidth}
       y="0"
       width={scoreWidth}
-      height={height}
-      fill="#787a80"
+      height={height / 2}
+      fill="#2f2f2f"
+    />
+    <rect
+      x={width - scoreWidth}
+      y={height / 2}
+      width={scoreWidth}
+      height={height / 2}
+      fill="#252525"
     />
     <text
       x="5"
@@ -63,7 +70,7 @@ export default ({
       y1={height / 2}
       x2={width}
       y2={height / 2}
-      style={{ stroke: 'rgb(68, 69, 73)', 'stroke-width': 1 }}
+      style={{ stroke: 'rgb(255, 255, 255)', 'stroke-width': 1 }}
     />
     <text
       x="182"
